39e4e3a4f310f7a9487e3df282a17b5f46d81db4
Alexa Goldman <ay0lexx@Alexas-MacBook-Pro-2.local>
2017-11-10 09:55:17 -0500

    progress

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 require('./db');
 const mongoose = require('mongoose');
-const User = mongoose.model('User');
-//const List = mongoose.model('List');
 const Log = mongoose.model('Log');
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -28,38 +26,8 @@ app.get('/create', (req, res) => {
 	res.render('create');
 });
 
-app.post('/create', function(req, res) {
-const newLog = new Log({
-		type: req.body.type,
-		description: req.body.description,
-		pace: req.body.pace,
-		goals: req.body.goals,
-		comments: req.body.comments,
-		location: req.body.location,
-		date: req.body.date
-});
-  newLog.save(function(err) {
-    if(err) {
-      console.log(err);
-    }
-    else {
-      res.redirect('/');
-    }
-  });
-});
-
-app.get('/:slug', function(req, res){
-  Log.findOne({slug: req.params.slug}, function(err, log){
-		if(err){
-			console.log(err);
-		}
-		res.render('index', {log: log});
-	});
-});
-
-
-// app.post('/', (req, res) => {
-// 	const newLog = new Log({
+// app.post('/create', function(req, res) {
+// const newLog = new Log({
 // 		type: req.body.type,
 // 		description: req.body.description,
 // 		pace: req.body.pace,
@@ -67,37 +35,19 @@ app.get('/:slug', function(req, res){
 // 		comments: req.body.comments,
 // 		location: req.body.location,
 // 		date: req.body.date
-// }, {
-//   _id: true
 // });
-// 	newLog.save((err) => {
-// 		if(err){
-// 			console.log(err);
-// 		}
-// 		res.redirect('/');
-// 	});
-// });
-
-
-//db.Person.remove({'last':'bob'})
-//db.Person.update({'first':'foo'}, {$set: {'last':'bar'}})
-
-
-// app.post('/', (req, res) => {
-// if(req.body.delete == true){
-// Log.findByIdAndRemove(Log._id, function(err1, doc1) { // doc here is actually err
-//         // handle err1
-//         console.log('findByIdAndRemove doc: ', doc1);
-//         Log.find({}, function(err2, docs) {
-//           console.log('Finding all: ', docs)
-//         })
-//       })
-// }
-// res.redirect('/');
+//   newLog.save(function(err) {
+//     if(err) {
+//       console.log(err);
+//     }
+//     else {
+//       res.redirect('/');
+//     }
+//   });
 // });
 
-// app.get('/index/:slug', (req, res) => {
-// 	Log.find({slug: req.params.slug}, (err, log) => {
+// app.get('/:slug', function(req, res){
+//   Log.findOne({slug: req.params.slug}, function(err, log){
 // 		if(err){
 // 			console.log(err);
 // 		}
@@ -110,4 +60,3 @@ app.get('/css/base.css', (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000);
-
