4fc6eb2189e848dc27e8f55a248e4832231cdbac
Alexa Goldman <ay0lexx@Alexas-MacBook-Pro-2.local>
2017-11-10 11:29:02 -0500

    progress

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 require('./db');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-
+const Log = mongoose.model('Log');
 app.use(bodyParser.urlencoded({extended:false}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,7 +23,45 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 
 app.get('/', (req, res) => {
-  res.render('index');
+	Log.find({}, (err, log) => {
+		if(err){
+			console.log(err);
+		}
+		res.render('index', {log: log});	
+	});
+});
+
+app.get('/create', (req, res) => {
+	res.render('create');
+});
+
+app.post('/create', function(req, res) {
+const newLog = new Log({
+		type: req.body.type,
+		description: req.body.description,
+		pace: req.body.pace,
+		goals: req.body.goals,
+		comments: req.body.comments,
+		location: req.body.location,
+		date: req.body.date
+});
+  newLog.save(function(err) {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      res.redirect('/');
+    }
+  });
+});
+
+app.get('/:slug', function(req, res){
+  Log.findOne({slug: req.params.slug}, function(err, log){
+		if(err){
+			console.log(err);
+		}
+		res.render('index', {log: log});
+	});
 });
 
 app.get('/css/base.css', (req, res) => {
