543a676a30bb452dfd4be8ac4b349af3b09458fc
Alexa Goldman <ay0lexx@Alexas-MacBook-Pro-2.local>
2017-11-10 03:05:41 -0500

    progress

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const [PORT, HOST] = [3000, '127.0.0.1'];
-const session = require('express-session');
-
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('./db');
 const mongoose = require('mongoose');
-const User = mongoose.model('User');
+//const User = mongoose.model('User');
 //const List = mongoose.model('List');
 const Log = mongoose.model('Log');
 
@@ -17,13 +15,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-const sessionOptions = {
-	secret: 'secret kitchen thang',
-	resave: true,
-	saveUninitialized: true
-};
-
-app.use(session(sessionOptions));
 
 app.get('/', (req, res) => {
 	Log.find({}, (err, log) => {
@@ -53,6 +44,8 @@ app.post('/', (req, res) => {
 		res.redirect('/');
 	});
 });
+
+
 //db.Person.remove({'last':'bob'})
 //db.Person.update({'first':'foo'}, {$set: {'last':'bar'}})
 
