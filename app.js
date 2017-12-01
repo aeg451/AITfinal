f4f276fd535c041e50498ae676598b4889e57664
Alexa Goldman <ay0lexx@Alexas-MBP-2.fios-router.home>
2017-12-01 17:53:08 -0500

    dependency errors?

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const path = require('path');
 //const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-//const expressValidator = require('express-validator');
+const expressValidator = require('express-validator');
 const connectFlash = require('connect-flash');
 const expressSession = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/passport', { useMongoClient: true});
+// mongoose.connect('mongodb://localhost/passport', { useMongoClient: true});
 
 //Init app
 const app = express();
-
+//require('./models/db');
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -37,21 +37,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //Express validator
-// app.use(expressValidator({
-//     errorFormatter: function (param, msg, value) {
-//         const namespace = param.split('.')
-//             , root = namespace.shift()
-//             , formParam = root;
-//         while(namespace.length){
-//             formParam += '[' + namespace.shift() + ']';
-//         }
-//         return {
-//             param: formParam,
-//             msg: msg,
-//             value: value
-//         };
-//     }
-// }));
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        const namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+        while(namespace.length){
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
 
 //Connect flash
 app.use(connectFlash());
@@ -68,15 +68,15 @@ app.use(function (req, res, next) {
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+app.listen(process.env.PORT || 3000);
+// //Set Port
+// app.set('port',3000);
 
-//Set Port
-app.set('port',3000);
-
-mongoose.connection.on('error', function(err) {
-    console.log('Mongodb is not running.');
-    process.exit();
-}).on('connected', function() {
-    app.listen(app.get('port'), function() {
-        console.log('Server started at : http://localhost:' + app.get('port'));
-    });
-});
+// mongoose.connection.on('error', function(err) {
+//     console.log('Mongodb is not running.');
+//     process.exit();
+// }).on('connected', function() {
+//     app.listen(app.get('port'), function() {
+//         console.log('Server started at : http://localhost:' + app.get('port'));
+//     });
+// });
