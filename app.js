f9eff78d8d2522b578fa0f6704a23d35fd79ab07
Alexa Goldman <ay0lexx@Alexas-MBP-2.fios-router.home>
2017-12-01 22:57:34 -0500

    half + done

--- app.js
+++ app.js
@@ -1,82 +1,228 @@
 const express = require('express');
+require('./db');
+const mongoose = require('mongoose');
+const Log = mongoose.model('Log');
+const Race = mongoose.model('Race');
+const User = mongoose.model('User');
+const app = express();
+
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+
 const path = require('path');
-const bodyParser = require('body-parser');
-const connectFlash = require('connect-flash');
-const expressSession = require('express-session');
-const cookieParser = require('cookie-parser');
+const publicPath = path.resolve(__dirname,'public');
+app.use(express.static('public'));
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const expressValidator = require('express-validator');
-const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/passport', { useMongoClient: true});
-
-//Init app
-const app = express();
-//require('./models/db');
-//View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-//Body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(cookieParser());
-
-//Set statuc folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Express Session
-app.use(expressSession({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave:true
-}));
-
-//Passport Init
+const flash = require("connect-flash");
+const expressSession = require('express-session');
+app.use(expressSession({secret: 'mySecret', resave:'false', saveUninitialized:'false'}));
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-
-//Express validator
-app.use(expressValidator({
-    errorFormatter: function (param, msg, value) {
-        const namespace = param.split('.')
-            , root = namespace.shift()
-            , formParam = root;
-        while(namespace.length){
-            formParam += '[' + namespace.shift() + ']';
-        }
-        return {
-            param: formParam,
-            msg: msg,
-            value: value
-        };
-    }
-}));
-
-//Connect flash
-app.use(connectFlash());
-
-//Global consts
-app.use(function (req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    res.locals.user = req.user || null;
-    res.locals.host = 'http://localhost:'+app.get('port');
-    next();
+app.set('view engine', 'hbs');
+///////////////////////////////////////////////////////////
+//PASSPORT
+///////////////////////////////////////////////////////////
+passport.serializeUser(function(user,done){
+    done(null,user._id);
 });
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err,user);
+    });
+});
+passport.use('local-login', new LocalStrategy({
+    passReqToCallback: true
+},
+    function(req, username, password, done){
+        User.findOne({'username':username}, function (err,user) {
+            if(err) return done(err);
+            if(!user){
+                console.log('Please enter a valid username instead of '+username );
+                return done(null,false, req.flash('message','No user found.'));
+            }
+            if(!user.isValidPassword(password)) {
+                console.log('Invlaid password!');
+                return done(null, false, req.flash('message', 'Invalid password.'));
+            }
+            return done(null,user);
+            }
+        );
+}));
+passport.use('local-signup', new LocalStrategy({
+        passReqToCallback: true
+    },
+    function(req, username, password, done){
+            process.nextTick(function(){
+            User.findOne({'username': username}, function (err, user) {
+                    if (err) {console.log('SignUp Error: '+err); return done(err);}
+                    if (user) {
+                        console.log('User already exists');
+                        return done(null, false, req.flash('message',"User already exists."));
+                    } else {
 
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
-app.listen(process.env.PORT || 3000);
-// //Set Port
-// app.set('port',3000);
-
-// mongoose.connection.on('error', function(err) {
-//     console.log('Mongodb is not running.');
-//     process.exit();
-// }).on('connected', function() {
-//     app.listen(app.get('port'), function() {
-//         console.log('Server started at : http://localhost:' + app.get('port'));
-//     });
-// });
+                        const newUser = new User()
+                        newUser.username = username;
+                        newUser.password = newUser.createHash(password);
+                        newUser.save(function(err){
+                            if(err) {console.log('Error: '+err); throw err;}
+                            console.log("SignUp Succeeded");
+                            return done(null,newUser);
+                        });
+                    }
+                });
+            });
+        }
+));
+///////////////////////////////////////////////////////////
+//LOGIN
+///////////////////////////////////////////////////////////
+app.get('/login', function(req,res){
+    res.render('login', {'user':req.user});
+});
+app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash : true
+    })
+);
+///////////////////////////////////////////////////////////
+//SIGNUP
+///////////////////////////////////////////////////////////
+app.get('/signup', function(req,res){
+    res.render('signup');
+});
+app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/login',
+        failureRedirect: '/signup',
+        failureFlash : true
+    })
+);
+///////////////////////////////////////////////////////////
+//LOGOUT
+///////////////////////////////////////////////////////////
+app.get('/logout', function(req, res){
+    const name = req.user.username;
+    console.log("LOGGIN OUT " + req.user.username)
+    req.logout();
+    res.redirect('/');
+    req.session.notice = "You have successfully been logged out " + name + "!";
+});
+///////////////////////////////////////////////////////////
+//INDEX
+///////////////////////////////////////////////////////////
+app.get('/', ensureAuthenticated, function (req, res) {
+    res.render('index');
+});
+///////////////////////////////////////////////////////////
+//VALIDATE AUTHENTICATION
+///////////////////////////////////////////////////////////
+function ensureAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }else{
+        res.redirect('/login');
+    }
+}
+///////////////////////////////////////////////////////////
+//LOG
+///////////////////////////////////////////////////////////
+app.get('/log',ensureAuthenticated,function (req,res) {
+    if(req.user) {
+        Log.find(function (err, log, count) {
+            res.render('log', {'log': log});
+        });
+    }
+    else
+        console.log("please login :)");
+});
+app.post('/log',function(req, res) {
+    if(req.body.delete){
+        if(req.body.delete){
+        Log.findByIdAndRemove(req.body.id, (err) => {  
+            if(err){
+                        console.log(err);
+                    }
+        });     
+    }
+    res.redirect('/log');   
+    }
+});
+///////////////////////////////////////////////////////////
+//CREATELOG
+///////////////////////////////////////////////////////////
+app.get('/createLog', ensureAuthenticated, function (req, res) {
+    res.render('createLog');
+});
+app.post('/createLog', function (req, res) {
+const newLog = new Log({
+        type: req.body.type,
+        description: req.body.description,
+        pace: req.body.pace,
+        goals: req.body.goals,
+        comments: req.body.comments,
+        location: req.body.location,
+        date: req.body.date
+});
+  newLog.save(function(err) {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      res.redirect('/log');
+    }
+  });
+});
+///////////////////////////////////////////////////////////
+//RACE
+///////////////////////////////////////////////////////////
+app.get('/race',ensureAuthenticated,function (req, res) {
+    if(req.user) {
+        Race.find(function (err, race, count) {
+            res.render('race', {'race': race});
+        });
+    }
+    else
+        console.log("please login :)");
+});
+app.post('/race',function(req, res) {
+    if(req.body.delete){
+        if(req.body.delete){
+        Race.findByIdAndRemove(req.body.id, (err) => {  
+            if(err){
+                        console.log(err);
+                    }
+        });     
+    }
+    res.redirect('/race');   
+    }
+});
+///////////////////////////////////////////////////////////
+//CREATERACE
+///////////////////////////////////////////////////////////
+app.get('/createRace', ensureAuthenticated, function (req, res) {
+    res.render('createRace');
+});
+app.post('/createRace',ensureAuthenticated, function (req, res) {
+const newRace = new Race({
+        distance: req.body.distance,
+        time: req.body.time,
+        comments: req.body.comments,
+        location: req.body.location,
+        date: req.body.date
+});
+  newRace.save(function(err) {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      res.redirect('/race');
+    }
+  });
+});
+///////////////////////////////////////////////////////////
+//LISTEN
+///////////////////////////////////////////////////////////
+app.listen(process.env.PORT||3000);
---
+++ db.js
@@ -0,0 +1,121 @@
+const mongoose = require('mongoose');
+const URLSlugs = require('mongoose-url-slugs');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+const passportLocalMongoose = require('passport-local-mongoose');
+///////////////////////////////////////////////////////////
+//USER
+///////////////////////////////////////////////////////////
+const User = new Schema({
+    race:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Log' }],
+    log:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Race' }],
+    username: String,
+    password: String
+});
+///////////////////////////////////////////////////////////
+//LOG
+///////////////////////////////////////////////////////////
+const Log = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  },
+  date: {
+    type: String, 
+    required: true
+  },
+  type: {
+    type: String, 
+    required: true
+  },
+  description: {
+    type: String, 
+    required: true
+  },
+  pace: {
+    type: String, 
+    required: true
+  },
+  goals: {
+    type: String, 
+    required: true
+  },
+  comments: {
+    type: String, 
+    required: true
+  },
+  location: {
+    type: String, 
+    required: true
+  }
+}, {
+  _id: true
+});
+///////////////////////////////////////////////////////////
+//RACE
+///////////////////////////////////////////////////////////
+const Race = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  },
+  date: {
+    type: String, 
+    required: true
+  },
+  distance: {
+    type: String, 
+    required: true
+  },
+  time: {
+    type: String, 
+    required: true
+  },
+  comments: {
+    type: String, 
+    required: true
+  },
+  location: {
+    type: String, 
+    required: true
+  }
+}, {
+  _id: true
+});
+//RaceSchema.plugin(URLSlugs('date race'));
+///////////////////////////////////////////////////////////
+//BCRYPT
+///////////////////////////////////////////////////////////
+User.methods.isValidPassword = function(password){
+    return bcrypt.compareSync(password,this.password);
+}
+User.methods.createHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+}
+///////////////////////////////////////////////////////////
+//MODELS
+///////////////////////////////////////////////////////////
+mongoose.model('Log', Log);
+mongoose.model('Race', Race);
+mongoose.model('User', User);
+///////////////////////////////////////////////////////////
+//CONNECTING
+///////////////////////////////////////////////////////////
+// is the environment constiable, NODE_ENV, set to PRODUCTION?
+if (process.env.NODE_ENV == 'PRODUCTION') {
+    // if we're in PRODUCTION mode, then read the configration from a file
+    // use blocking file io to do this...
+    const fs = require('fs');
+    const path = require('path');
+    const fn = path.join(__dirname, 'config.json');
+    const data = fs.readFileSync(fn);
+
+    // our configuration file will be in json, so parse it and set the
+    // connection string appropriately!
+    const conf = JSON.parse(data);
+    const dbconf = conf.dbconf;
+} else {
+    // if we're not in PRODUCTION mode, then use
+    dbconf = 'mongodb://localhost/project';
+}
+mongoose.connect(dbconf);
